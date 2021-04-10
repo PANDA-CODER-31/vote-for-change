ef7d97a2811ea3ad7b03b8df46e2930eb9d82535
Manglam Paliwal <paliwalmanglam16@gmail.com>
2021-04-11 04:06:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
                 <fieldset id="b1" class="city">
                     <h1 style="font-family:cursive">Login Credentials</h1>
                     <br><br><label for="name">NAME :</label>
-                    <input class="cls" type="text" id="name" name="name" required><br><br>
+                    <input class="cls" type="text" id="name" name="fname" required><br><br>
                     <label for="voterid">VOTER ID :</label>
                     <input class="cls" type="text" id="voterid" name="username" required><br><br>
                     <label for="password">PASSWORD:(Date of Birth)</label>
